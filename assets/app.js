0ad22a1ecda78526666c55188c8f0b3d009696ef
Djemai Samy <samy.djemai93@gmail.com>
2024-10-30 10:01:01 +0100

    Chengement couleur de la barre de navigation #3

--- assets/app.js
+++ assets/app.js
@@ -9,14 +9,3 @@ import './navbar.js';
 import './styles/app.css';
 
 
-// Navbar menu interaction
-const menuButton = document.querySelector('#navbar-menu-button');
-menuButton.addEventListener('click', () => {
-    const menu = document.querySelector('#navbar-menu');
-
-    if (menu.style.left == "0%") {
-        menu.style.left = "-100%";
-    } else {
-        menu.style.left = "0%";
-    }
-})
